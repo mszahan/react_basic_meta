f6a6ec2c5e4f0bfed85d4e718a019ca8087185e7
mszahan <mszahan1@gmail.com>
2023-10-29 20:30:44 +0600

    created first  functional components

---
+++ creatingcomponent/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header(){
+  return <h1> Hello React bulshit hola</h1>;
+}
+
+function App() {
+  return <Header/>;
+}
+
+export default App;
