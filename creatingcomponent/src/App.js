9410a61ec213333c0ae70ac47fb77f0b2de94545
mszahan <mszahan1@gmail.com>
2023-10-30 00:17:48 +0600

    created differents components and reused them

--- creatingcomponent/src/App.js
+++ creatingcomponent/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './componets/Nav';
 
 function Header(){
-  return <h1> Hello React bulshit hola</h1>;
+  return <h1 className='hello'> Hello Vues enemy </h1>
 }
 
 function App() {
-  return <Header/>;
+  return( 
+    <div> 
+      <Nav/>
+      <Header/>
+      <p className='para'>this is the paragraph</p>
+    </div>
+  ) ;
 }
 
 export default App;
